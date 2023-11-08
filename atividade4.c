73b8d0e3733db6bf31ce9789d2d8df6c75779a67
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-11-07 21:59:09 -0300

    Base da próxima atividade

---
+++ atividade4.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+float subtraiMatriz(int n, float m[], float x[]) {}
+
+float somaMatriz(int n, float m[], float x[]) {}
+
+float multiplicaMatriz(int n, float m[], float x[]) {}
+
+float determinanteMatriz(float m[], float x[]) {}
+
+int operacoesMatriciais(float m[], float x[], int opcao, int n) {}
+
+int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
